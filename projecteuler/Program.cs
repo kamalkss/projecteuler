02ba4f0e81a6802c004c72962466d78ce1d24497
kamalkss <kamalkss@hotmail.com>
2021-06-04 21:20:29 +0430

    Add project files.

---
+++ projecteuler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projecteuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new projecteulerProblem8();
+            x.Sum();
+        }
+    }
+}
